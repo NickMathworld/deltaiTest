5d1b18bc1c4ec8263d49218d8c92e131cb62e7b2
Brian Zuñiga Uribe <brian-kio@hotmail.com>
2020-04-07 02:09:48 -0500

    se agrega formato al response

--- utils/format.py
+++ utils/format.py
@@ -1,5 +1,7 @@
 import re
 def format_text(txt):
     ans = re.sub(r'(\n){1,}',' ', txt)
+    ans = re.sub(r'(\t){1,}',' ', ans)
+    ans = re.sub(r'(\r){1,}',' ', ans)
     ans = re.sub(r' {1,}',' ', ans)
     return ans
