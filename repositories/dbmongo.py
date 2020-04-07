251e73c384febad2aefa1e1309fd3df28e30cfa7
Brian Zuñiga Uribe <brian-kio@hotmail.com>
2020-04-06 22:15:37 -0500

    se usa una base de datos cloud

--- repositories/dbmongo.py
+++ repositories/dbmongo.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 class MongoDB:
     def insert_News(self,news,keywords,language):
-        client = MongoClient(port=27017)
+        client = MongoClient("mongodb+srv://nick:123@deltaicluster-072dm.mongodb.net/test?retryWrites=true&w=majority")
         db = client.deltAI
         aux = {'keywords':keywords,
                 'news': news,
@@ -10,13 +10,13 @@ class MongoDB:
         return result.inserted_id
     
     def find_News(self,keywords,language):
-        client = MongoClient(port=27017)
+        client = MongoClient("mongodb+srv://nick:123@deltaicluster-072dm.mongodb.net/test?retryWrites=true&w=majority")
         db = client.deltAI
         news = db.webScrapler.find_one({'keywords':keywords,'language':language})
         return news['news']
     
     def exits_News(self,keywords,language):
-        client = MongoClient(port=27017)
+        client = MongoClient("mongodb+srv://nick:123@deltaicluster-072dm.mongodb.net/test?retryWrites=true&w=majority")
         db = client.deltAI
         exist = db.webScrapler.find({'keywords':keywords,'language':language}).count()
         return exist>0
