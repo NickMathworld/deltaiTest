a70d0b1190830d310b2896cb7825e2e7118e7d0e
Brian Zuñiga Uribe <brian-kio@hotmail.com>
2020-04-07 00:28:00 -0500

    se corrige error en Procfile

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,6 @@ from whitenoise import WhiteNoise
 from api import app
 
 application = WhiteNoise(app)
-application.add_files('static/', prefix='static/')
+application.add_files('model/', prefix='model /')
+application.add_files('utils/', prefix='utils/')
+application.add_files('repositories/', prefix='repositories/')
